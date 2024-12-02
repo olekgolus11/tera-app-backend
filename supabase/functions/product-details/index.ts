1c69694a95545f7b3bffe5f04264ec7432ac2057
olekgolus11 <olekgolus02@gmail.com>
2024-12-02 18:48:52 +0100

    change product details to include isFavourite and isInCard

--- supabase/functions/product-details/index.ts
+++ supabase/functions/product-details/index.ts
@@ -1,23 +1,35 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { SupabaseService } from "../_shared/SupabaseService.ts";
+import { getBearerToken } from "../_shared/functions.ts";
 
-interface ProductDetails {
+interface VariantDetails {
     productId: string;
     color: string;
     sizes: {
         size: string;
         variantId: string;
+        isInCart: boolean;
     }[];
     imageUrl: string;
+    isFavorite: boolean;
 }
 
 Deno.serve(async (req) => {
-    const supabaseService = new SupabaseService();
-    const supabase = supabaseService.supabase;
+    let supabaseService;
+    let userId;
+
+    if (Deno.env.get("IS_DEV")) {
+        supabaseService = new SupabaseService();
+        userId = "e252c237-5d01-4b50-b7df-c1b8d5c7586b";
+    } else {
+        const token = getBearerToken(req);
+        supabaseService = new SupabaseService(token);
+        userId = (await supabaseService.getUser(token)).data.user?.id;
+    }
 
     const { productId, sex } = await req.json();
 
-    const { data, error } = await supabase
+    const { data, error } = await supabaseService.supabase
         .from("products_variants")
         .select(`
             id,
@@ -30,6 +42,8 @@ Deno.serve(async (req) => {
         .eq("product_id", productId)
         .eq("sex", sex);
 
+    console.log(data);
+
     if (error) {
         return new Response(
             JSON.stringify(error),
@@ -37,33 +51,67 @@ Deno.serve(async (req) => {
         );
     }
 
-    const productsDetails: ProductDetails[] = [];
+    const { data: favouritesData, error: favouritesError } =
+        await supabaseService.supabase
+            .from("favourites")
+            .select("variant_id")
+            .eq("user_id", userId);
 
-    for (const product of data) {
-        const productDetails = productsDetails.find(
-            (productDetails) => productDetails.color === product.color,
+    if (favouritesError) {
+        return new Response(
+            JSON.stringify(favouritesError),
+            { status: 403, headers: { "Content-Type": "application/json" } },
+        );
+    }
+
+    const { data: cartData, error: cartError } = await supabaseService.supabase
+        .from("baskets")
+        .select("variant_id")
+        .eq("user_id", userId);
+
+    if (cartError) {
+        return new Response(
+            JSON.stringify(cartError),
+            { status: 403, headers: { "Content-Type": "application/json" } },
+        );
+    }
+
+    const variantsDetails: VariantDetails[] = [];
+
+    for (const variant of data) {
+        const variantDetails = variantsDetails.find(
+            (productDetails) => productDetails.color === variant.color,
+        );
+        const isFavorite = favouritesData.some(
+            (favourite) => favourite.variant_id === variant.id,
+        );
+        const isInCart = cartData.some((cart) =>
+            cart.variant_id === variant.id
         );
 
-        if (productDetails) {
-            productDetails.sizes.push({
-                size: product.size,
-                variantId: product.id,
+        if (variantDetails) {
+            variantDetails.sizes.push({
+                size: variant.size,
+                variantId: variant.id,
+                isInCart,
             });
         } else {
-            productsDetails.push({
-                productId: product.product_id,
-                color: product.color,
+            variantsDetails.push({
+                productId: variant.product_id,
+                color: variant.color,
                 sizes: [{
-                    size: product.size,
-                    variantId: product.id,
+                    size: variant.size,
+                    variantId: variant.id,
+                    isInCart,
                 }],
-                imageUrl: product.image_url,
+                imageUrl: variant.image_url,
+                isFavorite,
             });
         }
     }
 
     return new Response(
-        JSON.stringify(productsDetails),
+        JSON.stringify(variantsDetails),
         { headers: { "Content-Type": "application/json" } },
     );
 });
